b122308d7bef2835e9adf9e5850da2b58d0d6a90
Marius-creator <marius.martinez40@gmail.com>
2019-10-21 14:31:50 +0200

    first

---
+++ lib/controller.rb
@@ -0,0 +1,31 @@
+require 'bundler'
+Bundler.require
+
+require 'gossip'
+
+class ApplicationController < Sinatra::Base
+  get '/' do
+    erb :index, locals: {gossips: Gossip.all}
+  end
+    get '/gossips/new/' do
+    erb :new_gossip
+  end
+  post '/gossips/new/' do
+    puts "Ce programme ne fait rien pour le moment, on va donc afficher un message dans le terminal"
+    Gossip.new(params["gossip_author"],params["gossip_content"]).save
+    redirect '/'
+  end
+  get '/gossips/:id' do
+    # matches "GET /hello/foo" and "GET /hello/bar"
+    # params['name'] is 'foo' or 'bar'
+    "Voici le potin : #{params['id']}!"
+  end
+end
+
+  #   post '/gossips/new/' do
+  #   puts "Salut, je suis dans le serveur"
+  #   puts "Ceci est le contenu du hash params : #{params}"
+  #   puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params['gossip_author']}"
+  #   puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params['gossip_content']}"
+  #   puts "Ça déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
+  #   end
